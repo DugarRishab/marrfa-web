924b330be2b82cca033edc4481ab08ba14a8318d
Abhijit <artistrup07@gmail.com>
2024-10-05 23:58:22 +0530

    [UPDATE]: Making Blogs Page responsive

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import "./Heading.css";
 
-const Heading = ({heading, sub, color = 'white', align = 'center', size=2.5}) => {
+const Heading = ({heading, sub, color = 'white', align = 'center', size=2.5, style = {}}) => {
 	return ( 
 		<div className="section-heading" style={{
 			color: color,
 			alignItems: align == 'left' ? 'flex-start' : align == 'right' ? 'flex-end' : 'center',
 			textAlign: align,
-			fontSize: `${size}rem`
+			fontSize: `${size}rem`,
+			...style
 		}}>
 			<p>{heading}</p>
 			<p className="sub">{ sub }</p>
--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -10,19 +10,20 @@ import Carousel from "../../components/carousel/Carousel";
 import CustomButton from "../../components/button/CustomButton";
 import BlogPagination from "../../components/BlogPagination/BlogPagination";
 import { BellOutlined, BorderHorizontalOutlined, SearchOutlined } from "@ant-design/icons";
-import { Divider, Select } from "antd";
+import { Divider, Input, Select } from "antd";
 
 const BlogsList = [];
+const mobile = 830
 
 import { NewsList } from "../../pages/Homepage/Homepage";
 import TrendingPost from "../../components/TrendingPost/TrendingPost";
-const Fin = []
+const Fin = [];
 
 for (let p of projectData) {
     BlogsList.push(<News data={p} />);
 }
-for(let q of financeData){
-    Fin.push(<News data={q} />)
+for (let q of financeData) {
+    Fin.push(<News data={q} />);
 }
 
 const Agri = [...NewsList, ...NewsList, ...NewsList];
@@ -45,52 +46,75 @@ const Category = ({ catname, activated, setActive }) => {
 
 const Categories = ["Finance", "Agriculture", "Real Estate", "Gold", "Stocks", "Markets"];
 const Catmap = {
-    "Finance": Finance,
-    "Agriculture": Agri,
+    Finance: Finance,
+    Agriculture: Agri,
     "Real Estate": Finance,
-    "Gold": Finance,
-    "Stocks": Agri,
-    "Markets": Agri
-}
-
+    Gold: Finance,
+    Stocks: Agri,
+    Markets: Agri,
+};
 
 const Blogs = () => {
     const [CatName, setCatName] = useState("Finance");
     const [Items, setItems] = useState(Finance);
+    const { innerWidth } = window
 
-    useEffect(()=>{
+    useEffect(() => {
         setItems(Catmap[CatName]);
     }, [CatName]);
 
     return (
         <div className="blogpage">
             <section className="mainblogs">
+                {innerWidth < mobile && <Input 
+                    className="custom-input"
+                    size="large" 
+                    placeholder="Looking for something specific?" 
+                    prefix={<SearchOutlined />} 
+                    style={{border: "2px solid #496ea1", background: "transparent", color: "#fff", marginBottom: "1rem"}}
+                />}
                 <div className="left">
-                    <Heading heading={"Latest on the blog"} align="left" />
+                    <Heading heading={"Latest on the blog"} align="left" style={{ marginTop: "0" }} />
                     <BlogCard />
                 </div>
                 <div className="right">
-                    <CustomButton text={"Looking for something specific?"} startIcon={<SearchOutlined />} style={{width: "100%", borderRadius: "2rem"}}/>
-                    <div className="heading">Trending Posts</div>
+                    {(innerWidth >= mobile) && (<CustomButton
+                        text={"Looking for something specific?"}
+                        startIcon={<SearchOutlined />}
+                        style={{ width: "100%", borderRadius: "2rem" }}
+                    />)}
+                    {(innerWidth >= mobile) && (<div className="heading">Trending Posts</div>)}
+                    {(innerWidth < mobile) && (<Heading heading={"Trending Posts"} align="left" style={{ margin: "2rem 0 0 0" }} />)}
+                    
                     <div className="posts-list">
-                        <TrendingPost 
-                            heading={"Headline headline headline"} 
-                            heading_byline={"20th Aug, 2024 | Finance"}
-                            desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse in sit minus quaerat ad ut explicabo quibusdam beatae aperiam ducimus?"}
+                        <Divider style={{ border: "1px solid #fff", margin: ".5rem", width: "100%" }} />
+                        <TrendingPost
+                            heading={"Headline headline headline"}
+                            date={"20th August, 2024"}
+                            tag={"Finance"}
+                            desc={
+                                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse in sit minus quaerat ad ut explicabo quibusdam beatae aperiam ducimus?"
+                            }
                             bannersrc={"/assets/banner/HomeBanner2.jpg"}
                         />
-                        <Divider style={{border: "1px solid #fff", "margin":".5rem"}}/>
-                        <TrendingPost 
-                            heading={"Headline headline headline"} 
-                            heading_byline={"20th Aug, 2024 | Finance"}
-                            desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse in sit minus quaerat ad ut explicabo quibusdam beatae aperiam ducimus?"}
+                        <Divider style={{ border: "1px solid #fff", margin: ".5rem 0 .5rem 0", width: "100%" }} />
+                        <TrendingPost
+                            heading={"Headline headline headline"}
+                            date={"20th August, 2024"}
+                            tag={"Finance"}
+                            desc={
+                                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse in sit minus quaerat ad ut explicabo quibusdam beatae aperiam ducimus?"
+                            }
                             bannersrc={"/assets/banner/HomeBanner2.jpg"}
                         />
-                        <Divider style={{border: "1px solid #fff", "margin":".5rem"}}/>
-                        <TrendingPost 
-                            heading={"Headline headline headline"} 
-                            heading_byline={"20th Aug, 2024 | Finance"}
-                            desc={"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse in sit minus quaerat ad ut explicabo quibusdam beatae aperiam ducimus?"}
+                        <Divider style={{ border: "1px solid #fff", margin: ".5rem", width: "100%" }} />
+                        <TrendingPost
+                            heading={"Headline headline headline"}
+                            date={"20th August, 2024"}
+                            tag={"Finance"}
+                            desc={
+                                "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse in sit minus quaerat ad ut explicabo quibusdam beatae aperiam ducimus?"
+                            }
                             bannersrc={"/assets/banner/HomeBanner2.jpg"}
                         />
                     </div>
