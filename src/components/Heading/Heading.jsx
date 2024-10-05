8b9b04dc32be4d44c32a66ea158f4eb9e3c6bd89
Soumyajit <sona832004@gmail.com>
2024-10-05 19:40:22 +0530

    [BUILD]: Created Blogs Page (INCOMPLETE)

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import "./Heading.css";
 
-const Heading = ({heading, sub, color = 'white', align = 'center'}) => {
+const Heading = ({heading, sub, color = 'white', align = 'center', size=2.5}) => {
 	return ( 
 		<div className="section-heading" style={{
 			color: color,
 			alignItems: align == 'left' ? 'flex-start' : align == 'right' ? 'flex-end' : 'center',
-			textAlign: align
+			textAlign: align,
+			fontSize: `${size}rem`
 		}}>
 			<p>{heading}</p>
 			<p className="sub">{ sub }</p>
