50f345648cdb40ae20ba4ca2ec2f1408bfbcd833
DugarRishab <suraj@marrfadiscovery.com>
2024-10-09 20:12:45 +0530

    API URL updated

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 const api = axios.create({
-	baseURL: "http://localhost:8000/api/v1",
-	// baseURL: "https://api.marrfa.com/api/v1",
+	// baseURL: "http://localhost:8000/api/v1",
+	baseURL: "https://api.marrfa.com/api/v1",
 	withCredentials: true,
 });
 
