f7c3d78ebc7119413eddfa47a272882a20584bf8
DugarRishab <suraj@marrfadiscovery.com>
2024-10-10 23:16:40 +0530

    [BUILD]: added new property cards

--- src/components/projectcard/ProjectCard-v2.jsx
+++ src/components/projectcard/ProjectCard-v2.jsx
@@ -1,102 +1,108 @@
 import React from "react";
 import "./ProjectCard-v2.css";
 
-import { WalletFilled, FormatPainterFilled, EnvironmentFilled, PhoneOutlined, WhatsAppOutlined } from "@ant-design/icons";
+import {
+	WalletFilled,
+	FormatPainterFilled,
+	EnvironmentFilled,
+	PhoneOutlined,
+	WhatsAppOutlined,
+} from "@ant-design/icons";
 
 import Whatsapp from "/assets/icons/whatsapp.svg";
 import CustomButton from "../button/CustomButton";
+import { Button } from "antd";
 
 export const Label = ({ label, value }) => {
-    return (
-        <div className="row">
-            <div className="brandicon">
-                <WalletFilled style={{ fontSize: "20px" }} />
-            </div>
-            <div className="data-part">
-                <div className="label">{label}:</div>
-                <div className="value">{value.toLocaleString().toUpperCase()}</div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="row">
+			<div className="brandicon">
+				<WalletFilled style={{ fontSize: "20px" }} />
+			</div>
+			<div className="data-part">
+				<div className="label">{label}:</div>
+				<div className="value">
+					{value.toLocaleString().toUpperCase()}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export const Location = ({ text }) => {
-    return (
-        <div className="location">
-            <EnvironmentFilled style = {{color: "var(--grey-text)"}} />
-            <div className="text">{text}</div>
-        </div>
-    );
+	return (
+		<div className="location">
+			<EnvironmentFilled style={{ color: "var(--grey-text)" }} />
+			<div className="text">{text}</div>
+		</div>
+	);
 };
 
 export const ButtonGroup = () => {
-    return (
-        <div className="base-section">
-            <CustomButton startIcon={<PhoneOutlined />}  fullWidth text={"Call"} />
-            <CustomButton startIcon={<WhatsAppOutlined />}  fullWidth text={"Whatsapp"} />
-        </div>
-    );
+	return (
+		<div className="base-section">
+			<CustomButton
+				startIcon={<PhoneOutlined />}
+				fullWidth
+				text={"Call"}
+			/>
+			<CustomButton
+				startIcon={<WhatsAppOutlined />}
+				fullWidth
+				text={"Whatsapp"}
+			/>
+		</div>
+	);
 };
 
 const Project = ({ data }) => {
-    return (
-        <div className="project">
-            <div
-                className="image-section"
-                style={{
-                    background: `url("${data.images.heroImg}")`,
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                    backgroundPosition: "center",
-                }}
-            >
-                <div className="image-tag">Developer</div>
-            </div>
-            <div className="info-section">
-                {/* <CustomButton
-                    size="medium"
-                    text={""}
-                    circled={true}
-                    className="icon"
-                    startIcon={<img src={Whatsapp} />}
-                />
-                <CustomButton
-                    size="medium"
-                    text={""}
-                    circled={true}
-                    className="icon"
-                    startIcon={
-                        <i className="material-icons" style={{ fontSize: "1.2rem" }}>
-                            call
-                        </i>
-                    }
-                /> */}
+	return (
+		<div className="project-card2">
+			<div className="image-section">
+				<img src={data.images.heroImg} alt="" />
+				<div className="image-tag">Residential</div>
+				<div className="marrfex-tag">4.5</div>
+				<div className="heading">
+					<div className="price">AED 20,00,000</div>
+					<div className="project-name">{data.name}</div>
+					<div className="location">Downtown, Dubai</div>
 
-                {/* <button className="icon material-icons">forum</button> */}
-
-                <div className="heading">
-                    <div className="project-name">{data.name}</div>
-                    <div className="rating">
-                        <div className=" number gradient-text">4.4</div>
-
-                        <Location text={"Downtown, dubai"} />
-                    </div>
-                </div>
-                <div className="stats">
-                    <Label label={"Start from"} value={data.type} />
-                    <Label label={"Completion date"} value={data.type} />
-                    <Label label={"Project Yield"} value={45200} />
-                    <Label label={"Area"} value={`${data.layout.size.value} ${data.layout.size.unit}`} />
-                </div>
-                {/* <Location text = {`${data.location.state}, ${data.location.country}`} /> */}
-            </div>
-            {/* <div className="base">
-                <CustomButton text={"Invest Now!"} fullWidth  />
-            </div> */}
-            <ButtonGroup />
-        </div>
-
-    );
+					<div className="base">
+						<div className="item">
+							<div className="icon">
+								<span className="material-symbols-outlined">
+									pageless
+								</span>
+							</div>
+							<div className="value">
+								{data.layout.size.value +
+									" " +
+									data.layout.size.unit}
+							</div>
+						</div>
+						<div className="item">
+							<div className="icon">
+								<span className="material-symbols-outlined">
+									event
+								</span>
+							</div>
+							<div className="value">{"NA"}</div>
+						</div>
+						<div className="item">
+							<div className="icon">
+								<span className="material-symbols-outlined">
+									pageless
+								</span>
+							</div>
+							<div className="value">
+								{data.metadata.completionDate || "NA"}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Project;
