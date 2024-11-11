dfb04ee7f2002066b102c9144360824bdaa6591a
rishab-marrfa <suraj@marrfadiscovery.com>
2024-11-12 03:13:57 +0530

    [BUG]: resolved some styling issues

--- src/components/AvatarCard/AvatarCard.jsx
+++ src/components/AvatarCard/AvatarCard.jsx
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import "./AvatarCard.css";
 
-const AvatarCard = ({ name, designation, dp }) => {
+const AvatarCard = ({ name, designation, dp, description }) => {
 	return (
 		<div className="avatar">
 			<img src={dp} />
 			<div className="whois">
 				<div className="name">{name}</div>
 				<div className="desig">{designation}</div>
+				<div className="description">
+					{description}
+				</div>
 			</div>
 		</div>
 	);
